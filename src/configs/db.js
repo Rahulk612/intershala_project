296eea6170769b3c2ad01c52aba2273016e0eec0
Rahulk612 <rahulkonda612@gmail.com>
2022-01-25 10:49:27 +0530

    Last Deploy

---
+++ src/configs/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+module.exports = () => {
+  return mongoose.connect(
+    process.env.MONGODB_URI ||
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.62qkq.mongodb.net/internshala`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+};
