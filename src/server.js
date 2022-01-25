30ee1097c828cbe0e9b364fba4f44354555adbcb
Rahulk612 <rahulkonda612@gmail.com>
2022-01-25 11:34:05 +0530

    server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-var methodOverride = require("method-override");
-
+// var methodOverride = require("method-override");
+const connect = require("./configs/db");
 const app = express();
 
-const Product = require("./models/products.model");
-const resume = require("./controllers/resumecontroller");
+// const Product = require("./models/products.model");
+// const resume = require("./controllers/resumecontroller");
 // const coverPage = require("./controllers/coverPage");
 
 const passport = require("./configs/passport")
 
 // vikash
-const productController = require("./controllers/product.controllers");
+// const productController = require("./controllers/product.controllers");
 
 
 app.use(express.urlencoded({extended:true}))
@@ -25,16 +25,16 @@ app.use(express.json());
 // } = require("./controllers/studentRegister.conteoller");
 
 // riyaz
-app.use(
-  methodOverride(function (req, res) {
-    if (req.body && typeof req.body === "object" && "_method" in req.body) {
-      // look in urlencoded POST bodies and delete it
-      var method = req.body._method;
-      delete req.body._method;
-      return method;
-    }
-  })
-);
+// app.use(
+//   methodOverride(function (req, res) {
+//     if (req.body && typeof req.body === "object" && "_method" in req.body) {
+//       // look in urlencoded POST bodies and delete it
+//       var method = req.body._method;
+//       delete req.body._method;
+//       return method;
+//     }
+//   })
+// );
 
 app.get("/", async (req, res) => {
   try {
@@ -45,17 +45,17 @@ app.get("/", async (req, res) => {
 });
 
 
-app.use("/internshala/resume", resume);
+// app.use("/internshala/resume", resume);
 // app.use("/coverPage", coverPage);
 
 
-const userController = require("./controllers/user.controller")
+// const userController = require("./controllers/user.controller")
 
 app.use(express.static("public"));
 
-app.use("/internshala",userController)
+// app.use("/internshala",userController)
 
-app.use("/internshala/products", productController);
+// app.use("/internshala/products", productController);
 
 
 
@@ -76,40 +76,40 @@ passport.deserializeUser(function (user, done) {
 // });
 
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-);
+// app.get(
+//   "/auth/google",
+//   passport.authenticate("google", { scope: ["email", "profile"] })
+// );
 
 
-app.get(
-  "/login/google",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-);
+// app.get(
+//   "/login/google",
+//   passport.authenticate("google", { scope: ["email", "profile"] })
+// );
 
 
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "/auth/google/failure",
-  }),
-  async (req, res) => {
-    // let data = await Product.find().lean().exec();
-    // // console.log("data:", data);
-    // dummyData = JSON.stringify(data);
-    let user = JSON.stringify(req.user.user);
-    return res.render("productPage", { user: user });
-  }
-);
+// app.get(
+//   "/auth/google/callback",
+//   passport.authenticate("google", {
+//     failureRedirect: "/auth/google/failure",
+//   }),
+//   async (req, res) => {
+//     // let data = await Product.find().lean().exec();
+//     // // console.log("data:", data);
+//     // dummyData = JSON.stringify(data);
+//     let user = JSON.stringify(req.user.user);
+//     return res.render("productPage", { user: user });
+//   }
+// );
 
 
-app.get("/auth/google/failure", (req,res) => {
-  return res.send("Failed")
-});
+// app.get("/auth/google/failure", (req,res) => {
+//   return res.send("Failed")
+// });
 
 
 
-const connect = require("./configs/db");
+
 
 app.listen(process.env.PORT || 5004, async () => {
   try {
